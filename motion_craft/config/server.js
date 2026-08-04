415e2a0cacb2600996add3c7b470a262090c8b23
Fritzi Joqueta FROIS <fritzi.froispro@gmail.com>
2026-08-04 15:54:11 +0200

    FIX: server.js

--- motion_craft/config/server.js
+++ motion_craft/config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  proxy: env.bool('IS_PROXIED', true),
+  proxy: {
+    koa: env.bool('IS_PROXIED', true),
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
