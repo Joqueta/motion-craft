d91590073a6eb7616f454bbdffa391dbec49a0c9
Fritzi Joqueta FROIS <fritzi.froispro@gmail.com>
2026-08-18 10:11:48 +0200

    feat: wire fritzi ContactPage to Strapi

--- pages/fritzi/contact.js
+++ pages/fritzi/contact.js
@@ -1,10 +1,11 @@
 import { Layout } from "../../components/fritzi/layout.js";
 import { ContactHero } from "../../components/fritzi/contact/contact-hero.js";
 import { ContactForm } from "../../components/fritzi/contact/contact-form.js";
-import { contactHeroMock } from "../../mocks/fritzi/contact-mock.js";
+import { fetchContactData } from "../../services/fritzi-content-service.js";
 
 export async function ContactPage() {
-    const hero = ContactHero(contactHeroMock);
+    const data = await fetchContactData();
+    const hero = ContactHero({ portrait: data.portrait });
     hero.appendChild(ContactForm({ sendLabel: "Send me" }));
 
     return Layout(hero);
